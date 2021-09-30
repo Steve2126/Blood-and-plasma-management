46a8b3d304a3572050da6fb53a1bc874f8fcfffc
Steve2126 <91477217+Steve2126@users.noreply.github.com>
2021-09-30 13:14:24 +0530

    Add files via upload

---
+++ bloodbank/bloodbank/bbdms/includes/header.php
@@ -0,0 +1,28 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Blood Doner & Blood receiver Management System</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Become a blood Donar</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search-donor.php">Search Blood</a>
+                    </li>
+        
+					<li class="nav-item">
+					   <a class="nav-link" href="plasma.php">Become a plasma doner</a>
+					   </li>
+					   <li class="nav-item">
+					   <a class="nav-link" href="covid beds.php"> Search plasma</a>
+					   </li>
+                 
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
